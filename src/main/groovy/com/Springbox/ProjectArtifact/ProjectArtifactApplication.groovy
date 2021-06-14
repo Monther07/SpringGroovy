fadafb244a0e919a387039f7798c59e797798d2e
MontherG <44753310+Monther07@users.noreply.github.com>
2021-06-14 22:49:57 +0000

    Add files via upload

---
+++ src/main/groovy/com/Springbox/ProjectArtifact/ProjectArtifactApplication.groovy
@@ -0,0 +1,13 @@
+package com.Springbox.ProjectArtifact
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProjectArtifactApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ProjectArtifactApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/Springbox/ProjectArtifact/ProjectArtifactApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.Springbox.ProjectArtifact
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
